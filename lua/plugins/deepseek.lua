7e1f154da64c3a9656e7d44af2bb885ad80a14c1
Chen Tuo <ch122633@126.com>
2025-07-30 14:51:33 +0800

    update to ratio window size

--- lua/plugins/deepseek.lua
+++ lua/plugins/deepseek.lua
@@ -4,9 +4,9 @@ return {
   "TeaAndCoffeeParty/deepseek.nvim",
   opts = {
     enabled = true,
-    window = { width = 100, height = 40, split_ratio = 0.2 },
+    window = { width = 0.6, height = 0.8, split_ratio = 0.2 },
     select_model = "google_gemini", -- model list "google_gemini", "aliyun_qwen", "deepseek"
-    timeout = 60000,
+    timeout = 80000,
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
