d3893de36ac69c242f81af6796521542875860ad
Chen Tuo <ch122633@126.com>
2025-08-13 10:46:11 +0800

    update settings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,3 +17,24 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.opt_local.softtabstop = 4
   end,
 })
+
+-- 创建一个专用于拼写检查的自动命令组
+local augroup = vim.api.nvim_create_augroup("SmartSpellCheck", { clear = true })
+
+-- 创建自动命令
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+  group = augroup,
+  pattern = "*", -- 匹配所有文件
+  callback = function()
+    -- 检查缓冲区是否可修改，以及不是特殊的缓冲区（如插件窗口）
+    if vim.bo.modifiable and vim.bo.buftype == "" then
+      -- 1. 启用本地拼写检查
+      vim.opt_local.spell = true
+
+      -- 2. 设置本地拼写检查的语言为：英语(en) 和 中日韩字符(cjk)
+      vim.opt_local.spelllang = { "en", "cjk" }
+    else
+      vim.opt_local.spell = false
+    end
+  end,
+})
