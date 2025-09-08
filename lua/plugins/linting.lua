58755994f4e0ed639976a87cb7794a370d82698e
xunshichentuo <chentuo@sprintray.cn>
2025-09-08 16:37:37 +0800

    add linting for cpp

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,21 @@
+-- lua/plugins/linting.lua
+
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufWritePre" },
+  config = function()
+    local lint = require("lint")
+    lint.linters_by_ft = {
+      cpp = { "cpplint" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
