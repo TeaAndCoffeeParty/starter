6d2e8711e8a49d5ed647036cb06f569220bb0128
Tuo <ch122633@126.com>
2025-11-11 17:14:18 +0800

    add windows powershell for default command

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,12 @@ local g = vim.g
 local opt = vim.opt
 opt.clipboard = "unnamedplus"
 
+if vim.fn.has("win32") == 1 then
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+  vim.g.terminal_shell = "pwsh"
+end
+
 return {}
