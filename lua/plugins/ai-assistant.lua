ff92df56b2365bce9ebf33ad28bd2667e903ae5d
Tuo <ch122633@126.com>
2025-12-15 15:48:19 +0800

    change default to qwen3-coder

--- lua/plugins/ai-assistant.lua
+++ lua/plugins/ai-assistant.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     enabled = true,
     window = { width = 0.6, height = 0.8, split_ratio = 0.2 },
-    select_model = "google_gemini", -- model list "google_gemini", "aliyun_qwen", "deepseek"
+    select_model = "modelscope", -- model list "google_gemini", "aliyun_qwen", "deepseek", "modelscope", "moonshot"
     timeout = 80000,
   },
   dependencies = {
