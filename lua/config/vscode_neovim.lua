b82ad634b66574ec2d8f8429842cf54790289be0
Tuo <ch122633@126.com>
2026-03-24 15:42:43 +0800

    refactor: streamline VS Code Neovim configuration and remove unused init file

--- lua/config/vscode_neovim.lua
+++ lua/config/vscode_neovim.lua
@@ -1,15 +1,15 @@
 -- ~/.config/nvim/lua/config/vscode_neovim.lua
 
-vim.notify("Loading VS Code specific Neovim configuration...", vim.log.levels.INFO)
 -- 只在 VSCode 环境中生效
 if not vim.g.vscode then
   return
 end
+vim.notify("Loading VS Code specific Neovim configuration...", vim.log.levels.INFO)
+vim.g.which_key_disabled = 1
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
-    pcall(vim.keymap.del, "n", "<leader>e")
     -- 切换主侧边栏（资源管理器）
     pcall(vim.keymap.del, "n", "<leader>e")
     vim.keymap.set(
