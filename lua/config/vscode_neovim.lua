dce0d4a7755577f5fa17a746976c4d7b9c935ed9
Tuo <ch122633@126.com>
2026-03-25 21:05:57 +0800

    add cmake and buffer command

--- lua/config/vscode_neovim.lua
+++ lua/config/vscode_neovim.lua
@@ -21,8 +21,12 @@ vim.api.nvim_create_autocmd("User", {
 
     -- === Git 源代码管理 ===
     pcall(vim.keymap.del, "n", "<leader>gg")
-    vim.keymap.set("n", "<leader>gg", "<Cmd>call VSCodeNotify('workbench.view.scm')<CR>", { desc = "Open Source Control", silent = true })
-
+    vim.keymap.set(
+      "n",
+      "<leader>gg",
+      "<Cmd>call VSCodeNotify('workbench.view.scm')<CR>",
+      { desc = "Open Source Control", silent = true }
+    )
 
     -- === 切换副侧边栏（聊天/输出） ===
     pcall(vim.keymap.del, "n", "<leader>ao")
@@ -36,13 +40,51 @@ vim.api.nvim_create_autocmd("User", {
     -- === 使用 VS Code CMake Tools 扩展的命令 ===
     vim.keymap.set("n", "<leader>mg", "<Cmd>call VSCodeNotify('cmake.configure')<CR>", { desc = "CMake Configure" })
     vim.keymap.set("n", "<leader>mb", "<Cmd>call VSCodeNotify('cmake.build')<CR>", { desc = "CMake Build" })
-    vim.keymap.set("n", "<leader>mr", "<Cmd>call VSCodeNotify('cmake.runTarget')<CR>", { desc = "CMake Run Target" })
+    vim.keymap.set(
+      "n",
+      "<leader>mr",
+      "<Cmd>call VSCodeNotify('cmake.launchTarget')<CR>",
+      { desc = "CMake Launch Target" }
+    )
     vim.keymap.set("n", "<leader>mc", "<Cmd>call VSCodeNotify('cmake.clean')<CR>", { desc = "CMake Clean" })
+    vim.keymap.set("n", "<leader>mt", "<Cmd>call VSCodeNotify('cmake.ctest')<CR>", { desc = "CMake Run Test" })
     vim.keymap.set(
       "n",
       "<leader>ms",
       "<Cmd>call VSCodeNotify('cmake.setBuildType')<CR>",
       { desc = "CMake Set Build Type" }
+    )
+
+    -- === 关闭当前编辑器 ===
+    vim.keymap.set(
+      "n",
+      "<leader>bd",
+      "<Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>",
+      { desc = "Close active editor" }
+    )
+
+    -- === 关闭其他编辑器（标签页） ===
+    vim.keymap.set(
+      "n",
+      "<leader>bo",
+      "<Cmd>call VSCodeNotify('workbench.action.closeOtherEditors')<CR>",
+      { desc = "Close other editors" }
+    )
+
+    -- === 关闭左侧所有编辑器 ===
+    vim.keymap.set(
+      "n",
+      "<leader>bl",
+      "<Cmd>call VSCodeNotify('workbench.action.closeEditorsToTheLeft')<CR>",
+      { desc = "Close editors to the left" }
+    )
+
+    -- === 关闭右侧所有编辑器 ===
+    vim.keymap.set(
+      "n",
+      "<leader>br",
+      "<Cmd>call VSCodeNotify('workbench.action.closeEditorsToTheRight')<CR>",
+      { desc = "Close editors to the right" }
     )
   end,
 })
