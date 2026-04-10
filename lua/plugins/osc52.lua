fe060eeaa6b1c6bd87ac500502fd3f9529b0b8b9
Tuo <ch122633@126.com>
2026-04-10 11:43:49 +0800

    update copy logical

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,18 +1,22 @@
 -- nvim-osc52
 return {
   "ojroques/nvim-osc52",
-  config = function()
-    require("osc52").setup({
-      max_length = 0, -- Maximum length of selection (0 for no limit)
-      silent = false, -- Disable message on successful copy
-      trim = false, -- Trim surrounding whitespaces before copy
+  opts = {
+    max_length = 0, -- 无限制
+    silent = true, -- 不显示消息（你已经有其他提示了）
+    trim = false,
+  },
+  config = function(_, opts)
+    require("osc52").setup(opts)
+    -- 只注册一个 autocmd，并确保不与其他剪贴板冲突
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      group = vim.api.nvim_create_augroup("osc52_yank", { clear = true }),
+      callback = function()
+        local event = vim.v.event
+        if (event.operator == "y" or event.operator == "d") and event.regname == "" then
+          require("osc52").copy_register("")
+        end
+      end,
     })
-    local function copy()
-      if (vim.v.event.operator == "y" or vim.v.event.operator == "d") and vim.v.event.regname == "" then
-        require("osc52").copy_register("")
-      end
-    end
-
-    vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
   end,
 }
